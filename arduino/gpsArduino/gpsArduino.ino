b27bbef14520ecc218205938110276b8b7d3b1ec
MatiasCionci <cioncimatias2@gmail.com>
2026-07-15 00:01:25 -0300

    Codigo gps funcionando

--- arduino/gpsArduino/gpsArduino.ino
+++ arduino/gpsArduino/gpsArduino.ino
@@ -1,21 +1,48 @@
 #include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 
-SoftwareSerial gps(4,3);
+// Mantenemos los mismos pines: Pin 4 (RX de Arduino, va al TX del GPS) y Pin 3 (TX de Arduino, va al RX del GPS)
+const int RXPin = 4;
+const int TXPin = 3;
+const int GPSBaud = 9600;
 
-char dato=' ';
+// Creamos el procesador de datos GPS
+TinyGPSPlus gps;
 
-void setup()
-{
- Serial.begin(115200);            
- gps.begin(9600); 
+// Creamos el puerto serie virtual
+SoftwareSerial gpsSerial(RXPin, TXPin);
+
+void setup() {
+  // Comunicación con la computadora (Monitor Serie)
+  Serial.begin(9600);
+  
+  // Comunicación con el GPS
+  gpsSerial.begin(GPSBaud);
+
+  Serial.println(F("--- Buscando satélites (Mantén el GPS al aire libre) ---"));
 }
 
+void loop() {
+  // Leemos los datos que llegan del GPS y se los pasamos a la librería para que los "traduzca"
+  while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      mostrarUbicacion();
+    }
+  }
+}
 
-void loop()
-{
-  if(gps.available())
-  {
-    dato=gps.read();
-    Serial.print(dato);
+void mostrarUbicacion() {
+  // Si la librería logra descifrar una ubicación válida (cuando el LED del GPS parpadee)
+  if (gps.location.isValid()) {
+    Serial.print(F("Latitud: "));
+    Serial.print(gps.location.lat(), 6); // Muestra 6 decimales de precisión
+    Serial.print(F(" | Longitud: "));
+    Serial.print(gps.location.lng(), 6);
+    
+    Serial.print(F(" | Satélites conectados: "));
+    Serial.println(gps.satellites.value());
+  } else {
+    // Si todavía no tiene señal de los satélites, te avisa aquí
+    Serial.println(F("Conexión OK, pero esperando señal de satélites..."));
   }
 }
